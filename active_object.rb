f0c62bc93a9c2f91fb8dbaf593401e07cddec852
jeff <jeff@MacBook-Pro-de-jeff.local>
2025-05-17 10:49:15 -0300

    map simple attribute

--- active_object.rb
+++ active_object.rb
@@ -1,5 +1,9 @@
 class ActiveObject
-  def code
-    42
-  end  
+  attr_reader :data
+  def initialize(data)
+    @data = data
+    data.each do |key, value|
+      self.class.define_method(key) { value }
+    end
+  end
 end
--- test/active_object_test.rb
+++ test/active_object_test.rb
@@ -2,6 +2,12 @@ require 'minitest/autorun'
 require_relative '../active_object'
 
 describe ActiveObject do
-  it { assert_equal 42, ActiveObject.new.code }
+  subject { ActiveObject.new(data) }
+
+  describe 'simple root attribute' do
+    let(:data) { { name: "Cormen's algorithm book - 2nd edition" } }
+
+    it { assert_equal "Cormen's algorithm book - 2nd edition", subject.name }
+  end
 end
 
