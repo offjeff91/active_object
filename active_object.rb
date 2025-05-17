156b5b3919ab5a863d5f0c340ee7552d82664b00
jeff <jeff@MacBook-Pro-de-jeff.local>
2025-05-17 12:33:47 -0300

    add count method

--- active_object.rb
+++ active_object.rb
@@ -4,6 +4,9 @@ class ActiveObject
     @data = data
     data.each do |key, value|
       self.class.define_method(key) { value }
+      if value.is_a?(Array)
+        self.class.define_method("count_#{key}") { value.count }
+      end
     end
   end
   def method_missing(method, *args)
--- test/active_object_test.rb
+++ test/active_object_test.rb
@@ -6,14 +6,28 @@ describe ActiveObject do
 
   describe 'simple root attribute' do
     book_name = "Cormen's algorithm book - 2nd edition"
+    authors = [ 'Maria', 'John', 'Jeff']
+    recommended_prices = [ 99.99, 79.99, 59.99, 119.99 ]
     cases = {
-      "with hash key" => { name: book_name },
-      "with string key" => { "name" => book_name }
+      "with hash key" => { 
+        name: book_name, 
+        authors: authors,
+        recommended_prices: recommended_prices
+      },
+      "with string key" => { 
+        "name" => book_name, 
+        "authors" => authors,
+        "recommended_prices" => recommended_prices
+      }
     }
     cases.each do |description, param|
       let(:data) { param }
       describe description do
         it { assert_equal book_name, subject.name }
+        it { assert_equal authors, subject.authors }
+        it { assert_equal recommended_prices, subject.recommended_prices }
+        it { assert_equal 3, subject.count_authors }
+        it { assert_equal 4, subject.count_recommended_prices }
         it { assert subject.has_name? }
         it { refute subject.has_price? }
         it { assert_raises(NoMethodError) { subject.has_methods? } }
